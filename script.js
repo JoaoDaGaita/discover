ec6a6a67600b6740c4d9d51ab80659ad869a1117
João <joaovsilva70@outlook.com>
2024-03-14 01:26:19 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function toggleModel() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  const image = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    image.setAttribute("src", "./assets/avatar-light.png")
+    image.setAttribute("alt", "light mode alt")
+  } else {
+    image.setAttribute("src", "./assets/avatar.png")
+    image.setAttribute("alt", "dark mode alt")
+  }
+}
